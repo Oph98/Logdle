ba85e324bf6367816cd074c9e58f40b8fac41832
Eph <149573474+Oph98@users.noreply.github.com>
2024-09-12 17:31:03 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -5,19 +5,18 @@ export const players = [
   { id: 3, name: "Muid", role: "dps", classColor: "#A9D271", class: "hunter", dps: 48652, rank: 3, rankColor: "purple" },
   { id: 4, name: "Micaíah", role: "dps", classColor: "#A9D271", class: "hunter", dps: 45455, rank: 4, rankColor: "blue" },
   { id: 5, name: "Âhrí", role: "dps", classColor: "#3FC7EB", class: "mage", dps: 44700, rank: 5, rankColor: "blue"  },
-  { id: 6, name: "Darharg", role: "dps", classColor: "#8788EE", class: "warlock", dps: 38660, rank: 6, rankColor: "green"  },
-  { id: 7, name: "Apollonaris", role: "dps", classColor: "#3FC7EB", class: "mage", dps: 35741, rank: 7, rankColor: "green"  },
-  { id: 8, name: "Bànânè", role: "dps", classColor: "#C41E3A", class: "deathknight", dps: 34293, rank: 8, rankColor: "blue"  },
-  { id: 9, name: "Bogrod", role: "dps", classColor: "#FFFFFF", class: "priest", dps: 31901, rank: 9, rankColor: "purple"  },
-  { id: 10, name: "Weakness", role: "dps", classColor: "#8788EE", class: "warlock", dps: 5470, rank: 10, rankColor: "grey"  },
+  { id: 6, name: "Apollonaris", role: "dps", classColor: "#3FC7EB", class: "mage", dps: 35741, rank: 6, rankColor: "green"  },
+  { id: 7, name: "Bànânè", role: "dps", classColor: "#C41E3A", class: "deathknight", dps: 34293, rank: 7, rankColor: "blue"  },
+  { id: 8, name: "Bogrod", role: "dps", classColor: "#FFFFFF", class: "priest", dps: 31901, rank: 8, rankColor: "purple"  },
+  { id: 9, name: "Weakness", role: "dps", classColor: "#8788EE", class: "warlock", dps: 5470, rank: 9, rankColor: "grey"  },
 
   // Tanks
-  { id: 11, name: "Tritzär", role: "tank", classColor: "#C79C6E", class: "warrior", dps: 26322, rank: 1, rankColor: "purple"   },
-  { id: 12, name: "Nightpaw", role: "tank", classColor: "#00FF98", class: "monk", dps: 18807, rank: 2, rankColor: "green"   },
+  { id: 10, name: "Tritzär", role: "tank", classColor: "#C79C6E", class: "warrior", dps: 26322, rank: 1, rankColor: "purple"   },
+  { id: 11, name: "Nightpaw", role: "tank", classColor: "#00FF98", class: "monk", dps: 18807, rank: 2, rankColor: "green"   },
 
 
   // Healers
-  { id: 13, name: "Lionlas", role: "healer", classColor: "#F48CBA", class: "paladin", dps: 59716, rank: 1, rankColor: "purple" },
-  { id: 14, name: "Lazorgun", role: "healer", classColor: "#0070DE", class: "shaman", dps: 55231, rank: 2, rankColor: "purple" },
-  { id: 15, name: "Aríya", role: "healer", classColor: "#FF7C0A", class: "druid", dps: 38447, rank: 3, rankColor: "blue" },
+  { id: 12, name: "Lionlas", role: "healer", classColor: "#F48CBA", class: "paladin", dps: 59716, rank: 1, rankColor: "purple" },
+  { id: 13, name: "Lazorgun", role: "healer", classColor: "#0070DE", class: "shaman", dps: 55231, rank: 2, rankColor: "purple" },
+  { id: 14, name: "Aríya", role: "healer", classColor: "#FF7C0A", class: "druid", dps: 38447, rank: 3, rankColor: "blue" },
 ]
